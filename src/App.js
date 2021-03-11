83bca732cda7c0fb7566ce44e89df956489c043b
sairamanan1810 <mksroct2000@gmail.com>
2021-03-11 12:25:20 +0530

    Login Page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import Loginpage from './components/Loginpage';
+import './App.css';
+function App() {
+  return (
+    <div>
+      <Loginpage/>
+    </div>
+  );
+}
+export default App;
